682c8d000a56e0a0845152682af4c9e34ba40b41
sathu0622 <sathushan622@gmail.com>
2025-05-03 22:27:05 +0530

    deploy

--- rest-countries-app/src/App.jsx
+++ rest-countries-app/src/App.jsx
@@ -1,3 +1,28 @@
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Home from './pages/Home';
+// import AuthFlow from './pages/AuthFlow';
+// import LoginForm from './pages/login';
+// import RegisterForm from './pages/register';
+// import OtpForm from './pages/otp';
+// import FavoritesPage from './pages/FavoritesPage';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/l" element={<LoginForm />} />
+//         <Route path="/register" element={<RegisterForm />} />
+//         <Route path="/otp" element={<OtpForm/>} />
+//         <Route path="/login" element={<AuthFlow />} />
+//         <Route path="/favorites" element={<FavoritesPage />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AuthFlow from './pages/AuthFlow';
